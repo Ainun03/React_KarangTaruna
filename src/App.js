19f89050801c11a732793dd9558ef3930f5bc1c0
Ainun_Rofiq H <ainunrofiq7@gmail.com>
2022-09-10 20:22:32 +0700

    uploud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import TentangPage from "./pages/tentang/TentangPage";
+import EventPage from "./pages/eventPage/AgendaPage";
+import KontakPage from "./pages/kontakPage/KontakPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import RegisterPage from "./pages/loginPage/Register";
+import KantorPage from "./pages/kantorSekPage/KantorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/tentang" element={<TentangPage />}/>
+        <Route path="/event" element={<EventPage />}/>
+        <Route path="/kontak" element={<KontakPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/kantor-sekretariat" element={<KantorPage />}/>
+      </Routes>
+  </BrowserRouter>
   );
 }
 
